6d18dbcc0e53894686a14152e2c917fa78490017
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2025-03-27 20:20:06 +0530

    Update portfoliojs.js

--- public/js/portfoliojs.js
+++ public/js/portfoliojs.js
@@ -15,3 +15,19 @@ if(cards.length>4)
 }
 
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+    slides[currentSlide].classList.add('active');
+    
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        
+        currentSlide = (currentSlide + 1) % slides.length;
+    
+        slides[currentSlide].classList.add('active');
+    }
+
+    setInterval(nextSlide, 3000);
+});
